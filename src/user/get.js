bb7ef41840c443cceaae099eafedefddb3df4045
22HOP3-Tergel <22hop3-tergel@KEKW.local>
2023-04-29 14:09:34 +0800

    get user done

---
+++ src/user/get.js
@@ -0,0 +1,26 @@
+const { marshall } = require("@aws-sdk/util-dynamodb");
+const db = require("../library/dynamodb");
+
+module.exports.getUser = async (event) => {
+  try {
+    const { Item: item } = await db.getItem({
+      TableName: "usersTable",
+      Key: marshall(
+        { id: event.pathParameters.id },
+        { removeUndefinedValues: true }
+      ),
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(item),
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "unsupported",
+      }),
+    };
+  }
+};
